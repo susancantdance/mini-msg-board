3dcc4fc693aafe2b0e794b6c6e1a383825bf4a87
susancantdance <susan.l.chen@gmail.com>
2024-12-05 14:22:39 -0800

    small typo with caps PORT

--- app.js
+++ app.js
@@ -12,4 +12,4 @@ app.use("/", index);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, "0.0.0.0", () => console.log(`running on port ${PORT}!`));
+app.listen(port, "0.0.0.0", () => console.log(`running on port ${port}!`));
