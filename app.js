246781107948491dbef6a83c00418c5d54ef4949
susancantdance <susan.l.chen@gmail.com>
2024-12-06 09:39:15 -0800

    changing port, changing start script

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const index = require("./routes/index");
 
 app.use("/", index);
 
-app.listen(Number(process.env.PORT) || 3000, "0.0.0.0", () =>
-  console.log(`running on port ${port}!`)
-);
+const port = process.env.PORT || 3000;
+
+// Listen on `port` and 0.0.0.0
+app.listen(port, "0.0.0.0", function () {
+  console.log(`listening on ${port}`);
+});
