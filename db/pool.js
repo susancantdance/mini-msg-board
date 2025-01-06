588f0659c4cb8f3bad03c04356ac3ab807909a0f
susancantdance <susan.l.chen@gmail.com>
2025-01-06 14:19:35 -0800

    changing connection string

--- db/pool.js
+++ db/pool.js
@@ -4,9 +4,6 @@ require("dotenv").config();
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
 module.exports = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: Number(process.env.PGPORT),
+  connectionString:
+    "postgresql://${{PGUSER}}:${{POSTGRES_PASSWORD}}@${{RAILWAY_TCP_PROXY_DOMAIN}}:${{RAILWAY_TCP_PROXY_PORT}}/${{PGDATABASE}}",
 });
