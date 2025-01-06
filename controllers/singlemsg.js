3eeea03d20cf5195f5e617f5ffebee3dc76e951d
susancantdance <susan.l.chen@gmail.com>
2025-01-06 13:59:20 -0800

    testing env variable, fixed paths

--- controllers/singlemsg.js
+++ controllers/singlemsg.js
@@ -1,5 +1,7 @@
 const asyncHandler = require("express-async-handler");
-const messages = require("../messages");
+const db = require("../db/queries");
+const messages = db.getAllMessages();
+//const messages = require("../messages");
 
 const singlemsg = asyncHandler(async (req, res) => {
   const userId = req.query.user;
