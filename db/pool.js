1411f8f5fc2d483de2fa48415b8e0d623fda46f8
susancantdance <susan.l.chen@gmail.com>
2025-01-06 14:35:37 -0800

    trying exposed address

--- db/pool.js
+++ db/pool.js
@@ -1,8 +1,9 @@
 const { Pool } = require("pg");
-require("dotenv").config();
+//require("dotenv").config();
 
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
 module.exports = new Pool({
-  connectionString: DATABASE_URL,
+  connectionString:
+    "postgresql://postgres:WojBOjFFIVVFZyinOIFLHnpfDzLXXwCU@postgres.railway.internal:5432/railway",
 });
