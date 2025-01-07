ccdae32c722079c2912066bba22dc2687f13b35b
susancantdance <susan.l.chen@gmail.com>
2025-01-06 19:47:49 -0800

    trying process.env.DB_URL

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,7 @@
 const { Pool } = require("pg");
-require("dotenv").config();
 
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
 module.exports = new Pool({
-  connectionString:
-    "postgresql://postgres:WojBOjFFIVVFZyinOIFLHnpfDzLXXwCU@postgres.railway.internal:5432/railway",
+  connectionString: process.env.DATABASE_URL,
 });
